dc7c0eec36f441ff4ba32229bf6ea144e9bc2508
Balázs Laczkó <laczko.balazs89@gmail.com>
2019-10-02 09:51:22 +0200

    gittie file modified with joke

--- gittie.py
+++ gittie.py
@@ -1,4 +1,15 @@
-def inroduce():
+import time
+
+
+def introduce():
     print("Hello, I'm Gittie!")
 
+def joke():
+    print('Why did the chicken cross the road?')
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print('To get to the other side')
+
 introduce()
+joke()
